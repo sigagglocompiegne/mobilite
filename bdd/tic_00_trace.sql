c50016937266c8cffd98491a5f044cf79eb59547
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-04-15 17:03:57 +0200

    Create tic_00_trace.sql

---
+++ bdd/tic_00_trace.sql
@@ -0,0 +1,20 @@
+/*TIC V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* tic_00_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Bodet Grégory */
+
+/*  
+ 
+  Liste des dépendances :
+  schéma          | table                 | description                                                   | usage
+*/
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+
+2021-04-15 : GB / initialisation du code
+
+
